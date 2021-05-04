9bf7859cb0bea43ce668175effd328871c8cddb1
mhendouf <mhendouf@gmail.com>
2021-05-04 03:36:17 +0200

    1er commit and push

--- ActiGestion/src/main/java/org/sid/ActiGestionApplication.java
+++ ActiGestion/src/main/java/org/sid/ActiGestionApplication.java
@@ -5,9 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ActiGestionApplication {
-
+//moh
 	public static void main(String[] args) {
-		SpringApplication.run(ActiGestionApplication.class, args);
+		SpringApplication.run ( ActiGestionApplication.class , args );
 	}
 
 }
